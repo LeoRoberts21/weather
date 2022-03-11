101d2205c21a5a7c10fab6d92a36d81d3b26e6e8
Leo Roberts <leo.roberts21@gmail.com>
2022-03-11 00:12:45 +0000

    Java initial

---
+++ app.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', ()=> {
+    let long;
+    let lat;
+
+    if(navigator.geolocation){
+navigator.geolocation.getCurrentPosition(position => {
+    long = position.coords.longitude;
+    lat = position.coords.latitude;
+
+const proxy = "https //cors-anywhere.herokuapp.com/";
+const api = `https://www.weatherapi.com/my/fc1e39c17bb341e6894231306221003/${lat},${long}`;
+
+fetch(api)
+    .then(response =>{
+        return response.json();
+    })
+    .then(data =>{
+        console.log(data);
+        const{temperature, summary } = data.currently;
+
+    })
+});
+
+}
+});
